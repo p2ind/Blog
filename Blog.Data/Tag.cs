ffa85b7cd68a446e91ab57649b9424c8204c4136
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 03:50:48 +0530

    Adding the Tag.cs

---
+++ Blog.Data/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class Tag : BaseEntity
+    {
+        [StringLength(75)]
+        public string Title { get; set; }
+
+        [StringLength(100)]
+        public string MetaTitle { get; set; }
+
+        [StringLength(100)]
+        public string Slug { get; set; }
+        public string Content { get; set; }
+    }
+}
