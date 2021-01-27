3db5cdadf5985ec660f90f5625741274db7e89e8
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 03:36:27 +0530

    Add project files.

---
+++ Blog.Data/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class User : BaseEntity
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string MiddleName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(15)]
+        public string MobileNumber { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(32)]
+        public string Password { get; set; }
+        public DateTime  RegisteredAt { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string Intro { get; set; }
+        public string Profile { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
