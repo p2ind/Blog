d8d836ef5497819e90ca469960e9f33f38015006
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 03:43:12 +0530

    Adding the Post Classe in Blog.Data Project

---
+++ Blog.Data/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class Post : BaseEntity
+    {
+        public int AuthorId { get; set; }
+
+        [StringLength(75)]
+        public string Title { get; set; }
+
+        [StringLength(100)]
+        public string MetaTitle { get; set; }
+
+        [StringLength(100)]
+        public string Slug { get; set; }
+        public string Summary { get; set; }
+        public bool Published { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Content { get; set; }
+    }
+}
