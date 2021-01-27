a1b6b8fbc9dfe6ba067de5c24e9d13a0ebf19dbf
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 04:37:27 +0530

    Update entities

--- Blog.Data/PostComment.cs
+++ Blog.Data/PostComment.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Data
 {
+    [Table("PostComment")]
     public class PostComment 
     {
         [Key]
