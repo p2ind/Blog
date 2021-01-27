9b3fcc73ee5e22acd9f50e027389dec43bf498d6
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 03:47:43 +0530

    Update Post.cs and Add PostComment.cs file

--- Blog.Data/PostComment.cs
+++ Blog.Data/PostComment.cs
@@ -7,22 +7,14 @@ using System.Threading.Tasks;
 
 namespace Blog.Data
 {
-    public class Post : BaseEntity
+    public class PostComment : BaseEntity
     {
-        public int AuthorId { get; set; }
+        public int PostId { get; set; }
 
-        [StringLength(75)]
+        [StringLength(100)]
         public string Title { get; set; }
-
-        [StringLength(100)]
-        public string MetaTitle { get; set; }
-
-        [StringLength(100)]
-        public string Slug { get; set; }
-        public string Summary { get; set; }
         public Boolean Published { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
         public DateTime PublishedAt { get; set; }
         public string Content { get; set; }
     }
