19c5642b6f0cb26f28fe7f217c37073e57610e54
Pankaj Saxena <saxena.pankaj7055@gmail.com>
2021-01-28 04:34:16 +0530

    update the entities

--- Blog.Data/PostCategory.cs
+++ Blog.Data/PostCategory.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Data
 {
+    [Table("PostCategory")]
     public class PostCategory
     {
+        [ForeignKey("Post")]
         public string PostId { get; set; }
         public Post Post { get; set; }
+
+        [ForeignKey("Category")]
         public string CategoryId { get; set; }
         public Category Category { get; set; }
     }
--- Blog.Data/PostTag.cs
+++ Blog.Data/PostTag.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Data
 {
+    [Table("PostTag")]
     public class PostTag
     {
+        [ForeignKey("Post")]
         public string PostId { get; set; }
         public Post Post { get; set; }
+
+        [ForeignKey("Tag")]
         public string TagId { get; set; }
         public Tag Tag { get; set; }
     }
